7ffefaa1b8b25520e936ba656b310391d64db8b7
Juels2319 <41863048+Juels2319@users.noreply.github.com>
2018-11-18 20:43:25 +0500

    added cardValidation in input by button

--- cardForm/index.php
+++ cardForm/index.php
@@ -5,3 +5,61 @@
  * Date: 17.11.2018
  * Time: 13:25
  */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<title>Кнопка</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+
+    function fBefore(data) {
+        console.log(data);
+    }
+
+    function fSuccess(response) {
+        //var jsonResponse = JSON.parse(response);
+        console.log(response);
+    }
+
+    $(document).ready(function () {
+        $("button").bind("click", function () {
+            $.ajax({
+                type: "POST",
+                url: "process/validateCard.php",
+                //data: 'data=' + JSON.stringify({name: this.name, value: this.innerHTML}),
+                data: 'data=' + JSON.stringify({cardNumber: $('input#cardNumber')[0].value}),
+                beforeSend: fBefore(JSON.stringify({cardNumber: $('input#cardNumber')[0].value})),
+                success: fSuccess
+            });
+        });
+    });
+</script>
+<table class="table" border="5 px">
+    <tr><td><input type="text" id="cardNumber" placeholder="CARD NUMBER"></td></tr>
+    <tr><td><input type="text" id="cardHolder" placeholder="CARDHOLDER"></td></tr>
+    <tr><td><input type="text" id="cardDate" placeholder="DATE"></td></tr>
+    <tr><td><input type="text" id="cardCVV" placeholder="***"></td></tr>
+</table>
+<button id="button" name="button">CHECK VALIDATE</button>
+<style>
+    .table{
+        width: 1200px;
+    }
+    .table td{
+        width: 1000px;
+        height: 100px;
+    }
+    .input cardNumber{
+        size: 80px;
+        font-size: 30px;
+    }
+    .input cardCVV{
+        size: 20px;
+    }
+
+</style>
+</body>
+</html>
--- cardForm/process/validateCard.php
+++ cardForm/process/validateCard.php
@@ -16,28 +16,28 @@ function checkDouble($numb)
 
 //$card='0010203040506070';
 //$card='4111111111111111';
-$card = '4242424242424242';
+//$card = '4242424242424242';
 
 function isValidCard($card)
 {
 
     $cardArray = str_split($card);
     $length = count($cardArray);
-    echo $length;
-    echo PHP_EOL;
+    //echo $length;
+    //echo PHP_EOL;
     $sum = 0;
     for ($i = 0; $i < $length - 1; $i++) {
         if ($i % 2 == 0) {
             $sum += checkDouble($cardArray[$i] * 2);
         } else $sum += $cardArray[$i];
     }
-    echo $sum += $cardArray[$length - 1];
+    $sum += $cardArray[$length - 1];
     return ($sum % 10 == 0 ? true : false);
-
     //echo ' ' . ($isValid ? 'VALID ' : 'NOT VALID ');
 }
+$json = json_decode($_POST['data'], true);
+$cardNumber = $json['cardNumber'];
+$response = array('response' => isValidCard($cardNumber), 'dev'=> 'dev_text');
+echo json_encode($response);
 
-$isValid=isValidCard($card);
-echo $isValid;
 
-echo 'end';
